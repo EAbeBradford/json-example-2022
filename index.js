dc681836c81ffc65e8cde1f1809aaa0f1f2f2c90
Mx. Bradford <emma_bradford@milton.com>
2022-11-15 13:37:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import data from './rawBooks.json' assert { type: 'json' };
+console.log(data);
+
+// fetch('./rawBooks.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
